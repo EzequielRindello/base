cd94f64574e227d8ba9bd55a598cfa47623159b2
EzequielRindello <109447017+EzequielRindello@users.noreply.github.com>
2025-11-25 20:51:22 -0300

    build and img refactor for netlify

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,11 @@
+import img1 from '../assets/images/DSC_9354.jpg';
+import img2 from '../assets/images/DSC_9364.jpg';
+import img3 from '../assets/images/DSC_9366.jpg';
+import img4 from '../assets/images/DSC_9369.jpg';
+import img5 from '../assets/images/DSC_9370.jpg';
+
 const Gallery = () => {
-  const images = [
-    '../src/assets/images/DSC_9354.jpg',
-    '../src/assets/images/DSC_9364.jpg',
-    '../src/assets/images/DSC_9366.jpg',
-    '../src/assets/images/DSC_9369.jpg',
-    '../src/assets/images/DSC_9354.jpg',
-    '../src/assets/images/DSC_9370.jpg',
-  ];
+  const images = [img1, img2, img3, img4, img5];
 
   return (
     <section className="gallery-section">
